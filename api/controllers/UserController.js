14e21272b6e508e5c3d54591d4bdbb1d0508805e
Kandianne <kandipierre@gmail.com>
2016-08-16 11:26:30 -0400

    preparing to connect db

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -36,11 +36,57 @@ module.exports = {
 
   show : function (req, res, next) {
     User.findOne(req.param('id'), function (err, createdUser) {
-      if(err) return console.log(next(err));
+      if(err) return console.log(err);
       if(!createdUser) return next();
       res.view({
         user: createdUser
       })
+    })
+  },
+
+  index: function (req, res, next) {
+    User.find(function foundUsers (err, foundUsers) {
+      if(err) return console.log(err);
+
+      res.view({
+        users: foundUsers
+      })
+    })
+  },
+
+  edit: function (req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, specificUser) {
+      if(err) return console.log(err);
+      if(!specificUser) return next('User doesn\'t exist.');
+
+      res.view({
+        user: specificUser
+      })
+    })
+  },
+
+  updateUser: function (req, res, next) {
+    User.update(req.param('id'), req.params.all(), function sendEdits(err) {
+      if(err) {
+        console.log(err);
+        res.redirect('/user/edit/' + req.param('id'));
+      }
+
+      // alert('You have updated' + req.param('userName') + 'successfully!');
+      res.redirect('/user/show/' + req.param('id'));
+    });
+  },
+
+  deleteUser: function (req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, specificUser) {
+      if (err) return next(err);
+      if (!specificUser) return next('User doesn\'t exist.');
+
+      User.destroy(req.param('id'), function userDestroyed(err) {
+        if (err) return next(err);
+      });
+
+      res.redirect('/user');
     })
   }
 };
--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  // localDiskDb: {
+  //   adapter: 'sails-disk'
+  // },
 
   /***************************************************************************
   *                                                                          *
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  mysql: {
+    adapter: 'sails-mysql',
+    host: 'localhost',
+    user: 'YOUR_MYSQL_USER', //optional
+    password: 'YOUR_MYSQL_PASSWORD', //optional
+    database: 'YOUR_MYSQL_DB' //optional
+  }
 
   /***************************************************************************
   *                                                                          *
@@ -74,14 +74,13 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  // somePostgresqlServer: {
+  // postgres: {
   //   adapter: 'sails-postgresql',
-  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
+  //   host: 'localhost',
   //   user: 'YOUR_POSTGRES_USER', // optional
   //   password: 'YOUR_POSTGRES_PASSWORD', // optional
   //   database: 'YOUR_POSTGRES_DB' //optional
   // }
-
 
   /***************************************************************************
   *                                                                          *
